0b5d50beebf864d32881658644d032103ff8e90b
ovo-yeah <velek.martin@email.cz>
2022-08-18 22:43:13 +0200

    Minor CSS changed

--- src/components/images/main_background.tsx
+++ src/components/images/main_background.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export const MainBackground: React.FC = () => {
     return (
         <Image
+            className="background-half-white"
             src="/icons/pexels-anamul-rezwan-background.jpg"
             alt="czech-republic-flag"
             width="2560"
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -6,28 +6,25 @@ import { Button } from "@components";
 
 export const Main: React.FC = () => {
     return (
-        <div>
-            <div className="flex justify-evenly">
-                <div className="">
-                    <h1>Vnitrostátní doprava</h1>
-                    <p>Společnost Groeneveld-BEKA dodává to správné řešení pro každou dopravní aplikaci.<br/>
-                        Nehledě na to, zda se jde o flotilu nákladních vozidel nebo vysokozdvižné vozíky ve<br/>
-                        skladovací hale.
-                    </p>
-                    <p>Protože se skladovací haly od sebe velmi liší a systém Greensight lze perfektně<br/>
-                        přizpůsobit, používá se často pro vnitrozávodní dopravu. Pro větší vysokozdvižné vozíky<br/>
-                        dodává Groeneveld-BEKA širokou paletu kompaktních automatických
-                    </p>
-                </div>
-                <div className="">
-                    <h1>Kontaktujte nás!</h1>
-                    <p>Zanechte nám zprávu přes kontaktní formulář a my se Vám obratem ozveme zpět.</p>
-                    <Link href="/">
-                        <a>Kontakt</a>
-                    </Link>
-                </div>
+        <div className="background-image grid grid-cols-2 content-center place-content-evenly">
+            <div className="my-8 bg-white">
+                <h1 className="text-5xl font-bold special-color mb-16">Vnitrostátní doprava</h1>
+                <p className="text-lg font-medium">Společnost Groeneveld-BEKA dodává to správné řešení pro každou dopravní aplikaci.
+                    Nehledě na to, zda se jde o flotilu nákladních vozidel nebo vysokozdvižné vozíky ve
+                    skladovací hale.
+                </p>
+                <p className="text-lg font-medium">Protože se skladovací haly od sebe velmi liší a systém Greensight lze perfektně
+                    přizpůsobit, používá se často pro vnitrozávodní dopravu. Pro větší vysokozdvižné vozíky
+                    dodává Groeneveld-BEKA širokou paletu kompaktních automatických
+                </p>
             </div>
-            <MainBackground />
+            <div className="my-8 bg-white">
+                <h1 className="text-5xl font-bold special-color mb-16">Kontaktujte nás!</h1>
+                <p className="text-xl font-normal special-color">Zanechte nám zprávu přes kontaktní formulář a my se Vám obratem ozveme zpět.</p>
+                <Link href="/">
+                    <a className="font-bold text-2xl special-color">Kontakt</a>
+                </Link>
+            </div>
         </div>
     );
 };
